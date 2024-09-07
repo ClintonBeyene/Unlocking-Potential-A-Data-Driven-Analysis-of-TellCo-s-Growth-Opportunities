3bf9c890a26aed130760502d0d73fb8feac2c799
Clinton Beyene <clintonbeyene52@gmail.com>
2024-09-07 17:19:28 +0300

    User engagement analysis

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -61,3 +61,74 @@ def save_df_to_postgres(df, table_name):
 
     # Save DataFrame to PostgreSQL
     df.to_sql(table_name, engine, if_exists='replace', index=False)
+
+def check_duplicates(df):
+    """
+    Check for duplicate rows in a DataFrame and return a summary.
+    
+    Parameters:
+    - df: Pandas DataFrame
+    
+    Returns:
+    - DataFrame or str: Summary of duplicate rows or success message
+    """
+    # Find duplicate rows
+    duplicates = df[df.duplicated(keep='first')]
+    
+    if duplicates.empty:
+        return "Success: No duplicated values."
+    else:
+        # Get the first column name
+        first_column_name = df.columns[0]
+        
+        # Get the first column value from the duplicated rows
+        duplicates_summary = duplicates[[first_column_name]].copy()
+        duplicates_summary['Number of Duplicates'] = duplicates.groupby(first_column_name)[first_column_name].transform('count')
+        
+        # Drop duplicate rows from the summary
+        duplicates_summary.drop_duplicates(inplace=True)
+        
+        return duplicates_summary
+    
+def get_numeric_columns(df):
+    """
+    Get a list of column names with numeric data types from a DataFrame.
+    
+    Parameters:
+    - df: Pandas DataFrame
+    
+    Returns:
+    - numeric_columns: List of column names with numeric data types
+    """
+    numeric_columns = df.select_dtypes(include=['number']).columns.tolist()
+    return numeric_columns
+    
+def check_numeric_anomalies(df, column, lower_bound=None, upper_bound=None):
+    """
+    Check for numeric anomalies in a specific column of a DataFrame and return a summary.
+    
+    Parameters:
+    - df: Pandas DataFrame
+    - column: The specific column to check
+    - lower_bound: Lower bound for numeric anomalies (optional)
+    - upper_bound: Upper bound for numeric anomalies (optional)
+    
+    Returns:
+    - str or DataFrame: Success message or summary of anomalies
+    """
+    if df[column].dtype not in ['int64', 'float64']:
+        return f"Error: Column {column} is not numeric."
+    
+    if lower_bound is not None and upper_bound is not None:
+        anomalies = df[(df[column] < lower_bound) | (df[column] > upper_bound)]
+    else:
+        anomalies = df[~df[column].apply(lambda x: isinstance(x, (int, float)))]
+    
+    if anomalies.empty:
+        return "Success: No anomalies detected."
+    else:
+        anomalies_summary = pd.DataFrame({
+            'Column Name': [column],
+            'Number of Anomalies': [len(anomalies)]
+        })
+        return anomalies_summary
